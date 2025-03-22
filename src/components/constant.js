743b067d5e99a004acdbb2203a0e3c58fca128a2
Aditya Tiwari <149659121+AdityaTiwari1052@users.noreply.github.com>
2025-03-22 13:15:30 +0530

    Update constant.js

--- src/components/constant.js
+++ src/components/constant.js
@@ -1,6 +1,6 @@
 const constants = {
-    ZEGO_APP_ID: process.env.REACT_APP_ZEGO_APP_ID,
-    ZEGO_SERVER_SECRET: process.env.REACT_APP_ZEGO_SERVER_SECRET,
+    APP_ID: process.env.REACT_APP_ZEGO_APP_ID,
+    SERVER_SECRET: process.env.REACT_APP_ZEGO_SERVER_SECRET,
   };
   
   export default constants;
